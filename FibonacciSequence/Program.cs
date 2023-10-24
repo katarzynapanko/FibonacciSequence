25ef40456c0fabd39211cfd1a4aab4314f003230
kate2 <kate2@LAPTOP-JR1EQ2T5>
2023-10-24 22:01:01 +0200

    Add project files.

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+Console.WriteLine("Enter lenght of FIbonacci sequence");
+var length = GetFibonacciLength();
+Console.WriteLine("Great");
+var sequence = CreateFibonacciSequence(length);
+Console.WriteLine(sequence);
+
+static uint GetFibonacciLength()
+{
+    var n = Console.ReadLine();
+    uint length;
+    while (!uint.TryParse(n, out length) || length <= 1)
+    {
+        Console.WriteLine("Wrong input");
+        n = Console.ReadLine();
+    }
+    return length;
+}
+
+static string CreateFibonacciSequence(uint length)
+{
+    var x = 0UL;
+    var y = 1UL;
+    var sb = new StringBuilder($"{x} {y} ");
+    for (var i = 2; i < length; i++)
+    {
+        var z = x + y;
+        sb.Append($"{z} ");
+        x = y;
+        y = z;
+    };
+    return sb.ToString();
+}
